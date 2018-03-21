34185a0f6760a01c7db60718680affc1da080f7e
qvil <tshyeon@seowonintech.co.kr>
2018-03-21 15:53:02 +0900

    First commit befor eject for using decorator

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import { observalbe } from 'mobx';
+import { observer } from 'mobx-react'
+
+@observer class App extends Component {
+  @observable count = 0;
+
+  render() {
+    return (
+      <div className="App">
+        Counter: {this.count} <br />
+        <button onClick={this.handleIncrement}>+</button>
+        <button onClick={this.handleDecrement}>-</button>
+      </div>
+    );
+  }
+
+  handleIncrement = () => {
+    this.count++;
+  }
+  handleDecrement = () => {
+    this.count--;
+  }
+}
+
+export default App;
